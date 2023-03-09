15b72662aca23bed478ad700e45b1e780fe6d1da
Nandakishors369 <nandakishors8213@gmail.com>
2023-03-09 20:22:45 +0530

    updates

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,12 +1,9 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:kofuko_test/fetchdata.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -19,62 +16,64 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Get Data"),
+        title: const Text("Get Data"),
       ),
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                keyboardType: TextInputType.number,
-                controller: countController,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: countController,
+                ),
               ),
-            ),
-            ElevatedButton(
+              ElevatedButton(
                 onPressed: () {
                   count = countController.text;
                   countController.clear();
                   log(count.toString());
                   setState(() {});
                 },
-                child: Text("Get data")),
-            FutureBuilder(
-              future: getData(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: int.parse(count),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: CircleAvatar(
-                          radius: 20,
-                          child: Image.network(
-                              snapshot.data!.results[index].picture.medium),
-                        ),
-                        title: Row(
-                          children: [
-                            Text(snapshot.data!.results[index].name.first),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Text(snapshot.data!.results[index].name.last),
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
-            )
-          ],
+                child: const Text("Get data"),
+              ),
+              FutureBuilder(
+                future: getData(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: int.parse(count),
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            radius: 20,
+                            child: Image.network(
+                                snapshot.data!.results[index].picture.medium),
+                          ),
+                          title: Row(
+                            children: [
+                              Text(snapshot.data!.results[index].name.first),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(snapshot.data!.results[index].name.last),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
